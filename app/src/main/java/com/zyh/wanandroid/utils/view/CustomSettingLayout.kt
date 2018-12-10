1c61fd7999fe2951be1cbfed1dd0041cdfdf14d1
ZYH <376238783@qq.com>
2018-12-10 23:45:09 +0800

    zyh commit

--- app/src/main/java/com/zyh/wanandroid/utils/view/CustomSettingLayout.kt
+++ app/src/main/java/com/zyh/wanandroid/utils/view/CustomSettingLayout.kt
@@ -15,28 +15,33 @@ import com.zyh.wanandroid.R
  * Description :
  */
 class CustomSettingLayout : ConstraintLayout{
-    constructor(context: Context): super(context){
-        init(context,null)
+    constructor(context: Context): this(context,null){
     }
-    constructor(context: Context,attributeSet: AttributeSet): super(context,attributeSet){
-        init(context,attributeSet)
+    constructor(context: Context,attributeSet: AttributeSet?): this(context,attributeSet,0){
     }
-    constructor(context: Context,attributeSet: AttributeSet, defStyleAttr: Int): super(context,attributeSet,defStyleAttr){
+    constructor(context: Context,attributeSet: AttributeSet?, defStyleAttr: Int): super(context,attributeSet,defStyleAttr){
         init(context,attributeSet)
     }
 
     fun init(context: Context, attributeSet: AttributeSet?){
         LayoutInflater.from(context).inflate(R.layout.custom_setting_layout,null)
         val custom_tv = findViewById<TextView>(R.id.custom_tv)
-        val attributes = context.obtainStyledAttributes(attributeSet, R.styleable.custom_setting_attr)
+        val attributes = context.obtainStyledAttributes(attributeSet, R.styleable.customSettingLayout)
         if (attributes!=null){
-            val customTv = attributes.getString(R.styleable.CustomSettingLayout_text)
+            val customTv = attributes.getString(R.styleable.customSettingLayout_text)
             if (TextUtils.isEmpty(customTv))
                 custom_tv.text = customTv
+            val customTvSize = attributes.getFloat(R.styleable.customSettingLayout_textSize, 14.0F)
+            custom_tv.textSize = customTvSize
+            val customTvColor = attributes.getColor(R.styleable.customSettingLayout_textColor,Color.BLACK)
+            custom_tv.setTextColor(customTvColor)
+            val customTvHeight = attributes.getInt(R.styleable.customSettingLayout_layout_height, 48)
+            custom_tv.height = customTvHeight
+            val customTvDrawableLeft = attributes.getDrawable(R.styleable.customSettingLayout_drawableLeft)
+            if (customTvDrawableLeft != null)
+            custom_tv.setCompoundDrawables(customTvDrawableLeft,null,null,null)
 
-            val customTvColor = attributes.getColor(R.styleable.CustomSettingLayout_textColor,Color.BLACK)
-            if (customTvColor != 0)
-//                custom_tv.textColors = customTvColor
+         attributes.recycle()
         }
 
     }
